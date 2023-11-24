f36aa2b780f01b0b1034459ffd0b996558db4098
Alexandr <efimchiksasha@gmail.com>
2023-11-24 10:03:28 +0300

    tool: compile scripts

--- compiler/compile.ps1
+++ compiler/compile.ps1
@@ -1,4 +1,4 @@
 # foreach($folder in $args){
 #     docker run -i --rm -v "$(Get-Location):/test" -w /test ajiob/docker-xelatex-fonts:1.2.1 make -j4 -C "$folder" all ; make -C "$folder" clean 
 # }
-docker run -i --rm -v "$(Get-Location):/test" -w /test ajiob/docker-xelatex-fonts:1.2.1 make -j4 -C "src" all ; make -C "src" clean 
+make -C "src" cleanall ; docker run -i --rm -v "$(Get-Location):/test" -w /test ajiob/docker-xelatex-fonts:1.2.1 make -j4 -C "src" all ; make -C "src" clean 
